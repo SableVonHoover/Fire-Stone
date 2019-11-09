9945e6ae4a7304e1df24a14dfc7ca8c47af6a13a
ginkaku67 <nshiply76@rocketmail.com>
2019-11-09 10:44:17 -0600

    got rid of most errors in fighting

--- src/components/NFighting_Logic.js
+++ src/components/NFighting_Logic.js
@@ -1,9 +1,16 @@
 import React from "react";
-import "../utils/API";
-import "withRouter(Login)" from "SignIn.js";
+const 
+let character = {////Will add all the other stats
+    health: 100,
+    attackStat: 20,
+    defense:10,
+    //level: 45,
+    baseDamage: [
+        1, 1000000
+    ]
+};
 
-
-const opponent = {
+let opponent = {
     name: "Jerry",
     level: 2,
     hitpoints: 100,
@@ -28,50 +35,50 @@ export default primaryAttackFunction() {
     let secondaryButton = document.getElementById('attack-button-2');
     let gameMessage = document.getElementById('game-message');
  ////////////////////////////////
-    let primaryAttack = determineAttack(character.attack/TBLD/, opponent.defense/TBLD/, character.level/TBLD/, character.baseDamage[0]/TBLD/);//trade in atk stat and add in move.basedmg as param MORE PARAMS TO BE ADDED
-    opponent.health/TBLD/ -= primaryAttack;
+    let primaryAttack = determineAttack(character.attack, opponent.defense, character.level, character.baseDamage[0]);//trade in atk stat and add in move.basedmg as param MORE PARAMS TO BE ADDED
+    opponent.health -= primaryAttack;
     printToScreen();////Replace with component ref
-    if (isGameOver(opponent.health/TBLD/)) {
+    if (isGameOver(opponent.health)) {
         endGame("Player wins.")///Change to whatever fail screen state they want/TBL/
         return;
     };
-    primaryButton.disabled/TBL/ = true;
-    secondaryButton.disabled/TBL/ = true; /// so you can't attack until opponent attack has gone through
-    gameMessage.innerText/TBL/ = "Opponent is about to wreck your shit."
+    primaryButton.disabled = true;
+    secondaryButton.disabled = true; /// so you can't attack until opponent attack has gone through
+    gameMessage.innerText = "Opponent is about to wreck your shit."
  /////////////////////////////////
     setTimeout(() => {
-        let opponentAttack = determineAttack(opponent.attack/TBLD/, character.defense/TBLD/, opponent.level/TBLD/, opponent.baseDamage[0]/TBLD/);///same as playerAttack
-        character.health/TBLD/ -= opponentAttack;
+        let opponentAttack = determineAttack(opponent.attack, character.defense, opponent.level, opponent.baseDamage[0]);///same as playerAttack
+        character.health -= opponentAttack;
         printToScreen();
-        if (isGameOver(character.health/TBLD/)) {
+        if (isGameOver(character.health)) {
             endGame("Opponent wins.");
             return;
         }
-        primaryButton.disabled /TBL/= false;
-        secondaryButton.disabled/TBL/ = false;
+        primaryButton.disabled = false;
+        secondaryButton.disabled = false;
     }, 300);//////// ASK ADAM HOW LONG ANIMATIONS WILL LAST FOR CHARACTERS WILL LAST
  };
  const secondaryAttackFunction = () => {
-    let primaryButton = document.getElementById('attack-button-1');/TBL/
-    let secondaryButton = document.getElementById('attack-button-2');/TBL/
+    let primaryButton = document.getElementById('attack-button-1');
+    let secondaryButton = document.getElementById('attack-button-2');
     let gameMessage = document.getElementById('game-message');///for win/lose  /opponent about to attack messages/TBL/
  ////////////////////////////////
-    let playerAttack2 = determineAttack(character.attack/TBLD/, opponent.defense/TBLD/, character.level/TBLD/, character.baseDamage[1]/TBLD/);
-    opponent.health/TBLD/ -= playerAttack2;
+    let playerAttack2 = determineAttack(character.attack, opponent.defense, character.level, character.baseDamage[1]);
+    opponent.health -= playerAttack2;
     printToScreen();
-    if (isGameOver(opponent.health/TBLD/)) {
+    if (isGameOver(opponent.health)) {
         endGame("Player wins.")///Change to whatever fail screen state they want
         return;
     };
-    primaryButton/TBL/.disabled = true;
-    secondaryButton/TBL/.disabled = true; /// so you can't attack until opponent attack has gone through
-    gameMessage.innerText/TBL/ = "Opponent is about to wreck your shit."
+    primaryButton.disabled = true;
+    secondaryButton.disabled = true; /// so you can't attack until opponent attack has gone through
+    gameMessage.innerText = "Opponent is about to wreck your shit."
  /////////////////////////////////
     setTimeout(() => {
-        let opponentAttack = determineAttack(opponent.attack/TBLD/, character.defense/TBLD/, opponent.level/TBLD/, opponent.baseDamage[0]/TBLD/);///same as playerAttack
-        character.health/TBLD/ -= opponentAttack;
-        printToScreen();/TBL/
-        if (isGameOver(character.health/TBLD/)) {
+        let opponentAttack = determineAttack(opponent.attack, character.defense, opponent.level, opponent.baseDamage[0]);///same as playerAttack
+        character.health -= opponentAttack;
+        printToScreen();
+        if (isGameOver(character.health)) {
             endGame("Opponent wins.");
             return;
         }
@@ -83,11 +90,11 @@ export default primaryAttackFunction() {
 const determineAttack = (attack, defense, level, damage) => {
     return Math.floor(Math.random() * ((2 * level + 10) / 250) * (attack/defense) *  damage + 2);
  };
- const isGameOver = (health/TBL/) => {///pretty straightforward
+ const isGameOver = (health) => {///pretty straightforward
     return health/TBL/ <= 0;
  };
  const printToScreen = () => {///// modify to set where Adam wants them
-    document.getElementById('opponent-health').innerText/TBL/ = opponent.health/TBLD/;
-    document.getElementById('character-health').innerText /TBL/= character.health/TBLD/;
+    document.getElementById('opponent-health').innerText = opponent.health;
+    document.getElementById('character-health').innerText = character.health;
  }
  printToScreen();
