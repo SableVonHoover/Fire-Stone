ab139a5ce47ae0a5a290c3129c801917859fb8a9
Sable Hoover <SableVonHoover@gmail.com>
2019-11-14 18:06:27 -0600

    updated route, controller, api util

--- controllers/userController.js
+++ controllers/userController.js
@@ -21,6 +21,16 @@ module.exports = {
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err));
     },
+
+    //This is where we associate the character the user makes with the their user entry in the DB
+    updateCharacter: function (req, res) {
+        db.User
+            //Filtering user's by email and pushing the new charcter's ID to user's character field
+            .findOneAndUpdate({email: req.params.email}, { $push: { character: req.params.id } }, { new: true })
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+
     update: function (req, res) {
         db.User
             .findOneAndUpdate({ _id: req.params.id }, req.body)
