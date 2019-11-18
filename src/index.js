6101700fea0af587feaa00c059bd4efcb41d6115
Peter Tan <petert@oberd.com>
2019-11-18 16:18:28 -0600

    added healthbar and possible methods, css changes

--- src/index.js
+++ src/index.js
@@ -5,17 +5,41 @@ import ButtonActions from "./components/ComponentLink";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { AuthProvider } from "./js/auth.js";
+import { StyleSheet, ImageBackground } from "react-native";
+
+const styles = StyleSheet.create({
+  Container: {
+    flex: 1,
+    width: "100vw",
+    height: "100vh",
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
+    left: "50%",
+    top: "50%",
+    transform: "translate(-50%, -50%)"
+  }
+});
 
 ReactDOM.render(
-  <div>
-    <AuthProvider>
-      <Router>
-        <div>
-          <ButtonActions />
-        </div>
-      </Router>
-    </AuthProvider>
-  </div>,
+  <ImageBackground
+    // blurRadius={100}
+    source={{
+      uri:
+        "https://images.pexels.com/photos/539986/pexels-photo-539986.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+    }}
+    style={styles.Container}
+  >
+    <div>
+      <AuthProvider>
+        <Router>
+          <div>
+            <ButtonActions />
+          </div>
+        </Router>
+      </AuthProvider>
+    </div>
+  </ImageBackground>,
   document.getElementById("root")
 );
 
